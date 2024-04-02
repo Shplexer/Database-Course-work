76ba83098ded4738b3e3c86b1450017c10418546
Дыня Орехов <danaran319@gmail.com>
2024-04-02 19:03:29 +0300

    start

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<body>
+    <div id="main_body">
+        
+        
+    </div>
+</body>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+    background-color: #9FBBCC;
+}
+#main_body{
+    background-color: #7A9CC6;
+    top: 20px;
+    bottom: 0px;
+    left: 20px;
+    right: 20px;
+    position: absolute;
 }
 </style>
